9edc587001631b551ee6ef58db8f52820e214dcb
RichardZaduban <zadubanr2@gmail.com>
2020-03-22 10:58:53 +0100

    Uprava main

--- app/src/main/java/com/example/bakalarka10/MainActivity.java
+++ app/src/main/java/com/example/bakalarka10/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bakalarka10;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -11,48 +13,75 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     Cursor cursor;
-    ListView listView;
-    int myPermissionRequestCalendar = 1;
+    int myPermissionRequestCalendar = 0;
+    RecyclerView recyclerView;
+    Adapter adapter;
+    ArrayList<Event> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
+        recyclerView = findViewById(R.id.recyclerview);
+
+
 
         if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(MainActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
-        }
+                Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED);
         else {
             requestCalendarPermission();
         }
 
-        cursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, null, null, null, null);
+        final String[] EVENT_PROJECTION = new String[] {
+                CalendarContract.Events.TITLE,                          // 0
+                CalendarContract.Events.DESCRIPTION,                   // 1
+                CalendarContract.Events.EVENT_LOCATION,                    // 2
+                CalendarContract.Events.ACCOUNT_NAME                   // 3
+        };
+
+        final int PROJECTION_TITLE_INDEX = 0;
+        final int PROJECTION_DESCRIPTION_INDEX = 1;
+        final int PROJECTION_EVENT_LOCATION_INDEX = 2;
+        final int PROJECTION_ACCOUNT_NAME_INDEX = 3;
+
+
+        cursor = null;
+        ContentResolver cr = getContentResolver();
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+        //String selection = "((" + CalendarContract.Events.ACCOUNT_NAME + " = ?) AND ("
+               // + CalendarContract.Events.ACCOUNT_TYPE + " = ?) AND ("
+                //+ CalendarContract.Events.OWNER_ACCOUNT + " = ?))";
+
+
+        cursor = cr.query(uri, EVENT_PROJECTION, null, null, null);
         while (cursor.moveToNext()){
             if (cursor!=null){
-                int id_2 = cursor.getColumnIndex(CalendarContract.Events.TITLE);
-                int id_3 = cursor.getColumnIndex(CalendarContract.Events.DESCRIPTION);
-                int id_4 = cursor.getColumnIndex(CalendarContract.Events.EVENT_LOCATION);
-                int id_5 = cursor.getColumnIndex(CalendarContract.Events.ACCOUNT_NAME);
 
-                String titleValue = cursor.getString(id_2);
-                String descriptionValue = cursor.getString(id_3);
-                String  locationValue = cursor.getString(id_4);
-                String nameValue = cursor.getString(id_5);
+                String titleValue = cursor.getString(PROJECTION_TITLE_INDEX);
+                String descriptionValue = cursor.getString(PROJECTION_DESCRIPTION_INDEX);
+                String locationValue = cursor.getString(PROJECTION_EVENT_LOCATION_INDEX);
+                String nameValue = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
 
+                Event event = new Event(titleValue, descriptionValue,null,null);
 
-                String[] events = {titleValue,descriptionValue,locationValue,nameValue};
+                 events = new ArrayList<>();
+                 events.add(event);
+                // events.add(descriptionValue);
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,events);
-                listView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+                adapter = new Adapter(this,events);
+                recyclerView.setAdapter(adapter);
 
                              }
                                 }
@@ -65,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.READ_CALENDAR)){
             new AlertDialog.Builder(this)
                     .setTitle("Permission needed")
-                    .setMessage("This permmission is needed for app to load data from your calendar")
+                    .setMessage("This permission is needed for app to load data from your calendar")
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -84,15 +113,12 @@ public class MainActivity extends AppCompatActivity {
                     .show();
 
         }
-
         else
                 {
                     ActivityCompat.requestPermissions(this,
                             new String[]{Manifest.permission.READ_CALENDAR},
                             myPermissionRequestCalendar);
-
                 }
-
         }
 
     @Override

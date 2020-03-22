28dadaf029fe44bf968b83a1f41d0ccca3d6da4c
RichardZaduban <zadubanr2@gmail.com>
2020-03-22 11:48:47 +0100

    Uprava

--- app/src/main/java/com/example/bakalarka10/MainActivity.java
+++ app/src/main/java/com/example/bakalarka10/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bakalarka10;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -15,11 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import android.widget.Toast;
-
 import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
     Cursor cursor;
@@ -28,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     Adapter adapter;
     ArrayList<Event> events;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,56 +34,48 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
 
 
-
         if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED);
+                Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) ;
         else {
             requestCalendarPermission();
         }
 
-        final String[] EVENT_PROJECTION = new String[] {
-                CalendarContract.Events.TITLE,                          // 0
+
+        final String[] EVENT_PROJECTION = new String[]{
+                CalendarContract.Events.TITLE,                         // 0
                 CalendarContract.Events.DESCRIPTION,                   // 1
-                CalendarContract.Events.EVENT_LOCATION,                    // 2
+                CalendarContract.Events.EVENT_LOCATION,                // 2
                 CalendarContract.Events.ACCOUNT_NAME                   // 3
         };
 
-        final int PROJECTION_TITLE_INDEX = 0;
-        final int PROJECTION_DESCRIPTION_INDEX = 1;
-        final int PROJECTION_EVENT_LOCATION_INDEX = 2;
-        final int PROJECTION_ACCOUNT_NAME_INDEX = 3;
-
-
-        cursor = null;
+        //cursor = null;
         ContentResolver cr = getContentResolver();
         Uri uri = CalendarContract.Events.CONTENT_URI;
-        //String selection = "((" + CalendarContract.Events.ACCOUNT_NAME + " = ?) AND ("
-               // + CalendarContract.Events.ACCOUNT_TYPE + " = ?) AND ("
-                //+ CalendarContract.Events.OWNER_ACCOUNT + " = ?))";
-
 
         cursor = cr.query(uri, EVENT_PROJECTION, null, null, null);
-        while (cursor.moveToNext()){
-            if (cursor!=null){
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                if (cursor != null) {
 
-                String titleValue = cursor.getString(PROJECTION_TITLE_INDEX);
-                String descriptionValue = cursor.getString(PROJECTION_DESCRIPTION_INDEX);
-                String locationValue = cursor.getString(PROJECTION_EVENT_LOCATION_INDEX);
-                String nameValue = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+                    String titleValue = cursor.getString(cursor.getColumnIndex(CalendarContract.Events.TITLE));
+                    String descriptionValue = cursor.getString(cursor.getColumnIndex(CalendarContract.Events.DESCRIPTION));
+                    String locationValue = cursor.getString(cursor.getColumnIndex(CalendarContract.Events.EVENT_LOCATION));
+                    String nameValue = cursor.getString(cursor.getColumnIndex(CalendarContract.Events.ACCOUNT_NAME));
 
-                Event event = new Event(titleValue, descriptionValue,null,null);
+                    Event event = new Event(titleValue, descriptionValue, null, null);
 
-                 events = new ArrayList<>();
-                 events.add(event);
-                // events.add(descriptionValue);
+                    events = new ArrayList<>();
+                    events.add(event);
 
-                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-                adapter = new Adapter(this,events);
-                recyclerView.setAdapter(adapter);
+                    adapter = new Adapter(this, events);
+                    recyclerView.setAdapter(adapter);
 
-                             }
-                                }
+                }
+            }
+
+        }
     }
 
     //tato metoda sluzi na poziadanie o povolenia aplikacie
